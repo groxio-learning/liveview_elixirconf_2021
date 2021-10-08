5c33cdd27f9f132129907c8f7016609a02ea622d
Bennie Rosas <ben@aliencyb.org>
2021-10-08 12:16:28 -0500

    inital implementation of Eraser module

--- memorex/lib/memorex/eraser.ex
+++ memorex/lib/memorex/eraser.ex
@@ -1,13 +1,29 @@
 defmodule Memorex.Eraser do
   def new(phrase, steps) do
-    %{plan: [], phrase: ""}
+    size = String.length(phrase)
+    chunk_size = div(size + steps - 1, steps)
+    plan = 0..(size - 1) |> Enum.shuffle |> Enum.chunk_every(chunk_size)
+    %{plan: plan, phrase: phrase}
   end
 
-  def erase(eraser) do
-    # returns eraser
+  def erase(%{plan: []} = eraser) do
+    eraser
   end
 
-  def as_string(eraser) do
-    # something
+  def erase(%{plan: [head_plan | tail_plan], phrase: phrase}) do
+    new_phrase = enact_plan_step(head_plan, phrase)
+
+    %{plan: tail_plan, phrase: new_phrase}
+  end
+
+  def as_string(%{phrase: phrase}), do: phrase
+
+  defp enact_plan_step(plan_step, phrase) do
+    graphemes = String.graphemes(phrase)
+
+    Enum.reduce(plan_step, graphemes, fn (plan_index, acc) ->
+      List.replace_at(acc, plan_index, "_")
+    end)
+    |> Enum.join()
   end
 end
