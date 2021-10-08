6bb6fa9adbeafd9dd6c11adc1f79e53a42235443
Bennie Rosas <ben@aliencyb.org>
2021-10-08 11:29:22 -0500

    stub Eraser module

---
+++ memorex/lib/memorex/eraser.ex
@@ -0,0 +1,13 @@
+defmodule Memorex.Eraser do
+  def new(phrase, steps) do
+    %{plan: [], phrase: ""}
+  end
+
+  def erase(eraser) do
+    # returns eraser
+  end
+
+  def as_string(eraser) do
+    # something
+  end
+end
